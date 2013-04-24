c5a02765b1486ba1e112245cc30cd59f9f403550
diegoescobar <diego@diegoescobar.ca>
2013-04-24 17:36:13 -0400

    Functionallity Update
    Added CodeIgniter, forked from the main git repository

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,66 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class dashboard extends CI_Controller {
+
+	private $data;
+	function __construct(){
+		parent::__construct();
+		/*$this->load->model('variables');
+		$data = $this->variables->setup_vars();
+		$alias = $this->session->userdata('alias');
+		if ( $alias ) {
+			$user = $this->session->userdata('id');
+			//$data['user'] = $this->user_vars->user_data($user);
+			//$data['notice_count'] = $this->_notifications( $user );
+		}else{
+			redirect('login', 'refresh');
+		}*/
+		//$this->data = $data;
+	}
+
+	/*private function _notifications( $user_id){
+		$timestamp = $this->session->userdata('timestamp');
+		$id = $this->session->userdata('id');
+		$notifi_query = $this->db->query('SELECT COUNT( * ) AS count FROM comments
+										JOIN posts ON comments.post = posts.id
+										WHERE user =  "'.$id.'"
+										AND comments.timestamp >  "'.$timestamp.'"');
+
+		$notification = $notifi_query->result();
+		$user = $this->session->userdata('id');
+		return $notification[0]->count;
+	}*/
+
+	public function _loader($menu = '', $page = '', $data){
+		$data['page'] = $page;
+		$data['menu'] = $menu;
+		$this->load->view('layout', $data);
+	}
+
+
+	public function index(){
+		$data = $this->data;
+		$data['js_files'] = array();
+		$data['title'] = 'Comicnerd - Index';
+		$this->_loader('navmenu', 'nav', $data);
+	}
+
+	public function dash(){
+		$data = $this->data;
+		$data['js_files'] = array();
+		$data['title'] = 'Comicnerd - Index';
+		$this->_loader('navmenu', 'dash', $data);
+	}
+
+
+	public function updates(){
+		//if notifications checked
+		$current = date("Y-m-d H:i:s");
+		$this->session->set_userdata('timestamp', $current );
+		$this->db->query('UPDATE users SET timestamp = "'. $current .'" WHERE id = "$user"');
+
+	}	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+<link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('images/nav/about@2x.png'); ?>">
+<link rel="apple-touch-startup-image" href="<?= base_url('images/splash/about.png'); ?>" media="screen and (max-device-width: 320px)" />
+<link rel="apple-touch-startup-image" media="(max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2)" href="<?= base_url('images/splash/about@2x.png'); ?>" />
+<title><?= $title ?></title>
+<link href="<?= base_url('styles/style.css'); ?>" rel="stylesheet" type="text/css">
+<link href="<?= base_url('styles/type.css'); ?>" rel="stylesheet" type="text/css">
+<link href="<?= base_url('styles/buttons.css'); ?>" rel="stylesheet" type="text/css">
+<link rel="stylesheet" media="only screen and (-webkit-min-device-pixel-ratio: 2)"	type="text/css" href="<?= base_url('styles/highdpi.css'); ?>" />
+
+<script type="text/javascript" src="<?= base_url('scripts/jquery.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('scripts/highdpi.js'); ?>"></script>
+<script type="text/javascript" src="<?= base_url('scripts/custom.js'); ?>"></script>
+
+</head>
+<body>
